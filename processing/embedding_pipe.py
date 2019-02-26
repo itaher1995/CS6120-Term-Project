742e09334ff94c9983dbe11c42ea82b94879b13f
fhooton <yourfriendforrest@gmail.com>
2019-02-26 08:57:17 -0500

    We in business

--- processing/embedding_pipe.py
+++ processing/embedding_pipe.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from time import time
 
 from gensim.models import Word2Vec
 
@@ -10,10 +12,36 @@ import config   # Read in hyperparameters from config so that we can control for
 def tokenize_strings(string):
 	return string.split()
 
+
+# Creates word embeddings using gensim Word2Vec
+def create_embeddings(texts, load_pretrained=False):
+	"""
+	Input: list of documents, where each document is tokenized
+	Output: Word embeddings for vocabulary
+	"""
+	start = time()
+
+	if load_pretrained:
+		# For if we want to include a pretrained vector later
+		pass
+	else:
+		model = Word2Vec(texts, size=config.DIM_EMBEDDING, sg=1, window=5, min_count=1, workers=4)
+
+	model.train(texts, total_examples=model.corpus_count, epochs=model.epochs)
+
+	print('Model trained in', (time()-start)/60, 'min')
+
+	return model
+
+
 def main():
 	# Columns: ['data', 'filenames', 'target_names', 'target', 'source', 'partition']
 	data = pd.read_pickle('../data/newsgroup.pkl')
-	print(config.EMBEDDING_SIZE)
+
+	temp = data.iloc[0:10]['data'].apply(tokenize_strings).tolist()
+	
+	create_embeddings(temp)
+
 
 if __name__ == "__main__":
 	main()
